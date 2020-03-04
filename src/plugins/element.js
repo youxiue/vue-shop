00df13337a000c6991df0b8e6e3966a3feb15627
youxiue <xfbliuli@qq.com>
2020-03-04 18:04:18 +0800

    登录页面 未完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 引入的组件只能写在一行
+import { Button, Form, FormItem, Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
