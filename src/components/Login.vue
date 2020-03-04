fe1e8bae6cee65b8e9eaaa9b6056a9731af5ea32
youxiue <xfbliuli@qq.com>
2020-03-04 22:07:53 +0800

    登录表单 校验

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,19 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!--登录表单区域-->
-      <el-form label-width="0px" class="login_form">
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
         <!--用户名-->
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="el-icon-user"></el-input>
         </el-form-item>
         <!--密码-->
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" type="password"  prefix-icon="el-icon-lock"></el-input>
         </el-form-item>
         <!--按钮-->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm()">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -26,7 +26,35 @@
 </template>
 
 <script>
-export default { name: 'Login' }
+export default {
+  data () {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      /// 表单验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮
+    resetLoginForm () {
+      // 说明: 在form 表单上添加 ref="loginFormRef" 属性 ,即可 通过this.$refs.loginFormRef 获取该对象.
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -44,26 +72,25 @@ export default { name: 'Login' }
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+  }
 
-    .avatar_box {
-      height: 130px;
-      width: 130px;
-      border: 1px solid #eee;
-      border-radius: 50%;
-      padding: 10px;
-      box-shadow: 0 0 10px #ddd;
-      position: absolute;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      background-color: #fff;
-
-      img {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        background-color: #eee;
-      }
-    }
+  .login_box .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+  }
+  .login_box .avatar_box img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
   }
 
   .login_form{
