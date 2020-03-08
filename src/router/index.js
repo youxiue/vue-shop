ce082a7ad3b96a462c6099a1569e9a69d68ebcb0
youxiue <xfbliuli@qq.com>
2020-03-08 14:25:05 +0800

    商品功能开发完成

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '@/components/power/Rights'
 import Roles from '@/components/power/Roles'
 import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
+import GoodsList from '@/components/goods/List'
+import AddGoods from '@/components/goods/AddGoods'
 
 Vue.use(VueRouter)
 
@@ -48,6 +50,14 @@ const routes = [
       {
         path: '/params',
         component: Params
+      },
+      {
+        path: '/goods',
+        component: GoodsList
+      },
+      {
+        path: '/goods/add',
+        component: AddGoods
       }
     ]
   }
